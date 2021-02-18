bb93d3a78ed0d84101dba4a933c1ba48961aee13
Marlowe <g00325096@gmit.ie>
2021-02-18 10:17:48 +0000

    added new line of code

--- src/main/java/ie/marlowe/calculator.java
+++ src/main/java/ie/marlowe/calculator.java
@@ -1,5 +1,6 @@
 package ie.marlowe;
 
+
 public class calculator {
     // This method will add two numbers.
     public int add(int num1, int num2){
--- src/test/java/ie/marlowe/CalculatorTest.java
+++ src/test/java/ie/marlowe/CalculatorTest.java
@@ -1,7 +1,16 @@
 package ie.marlowe;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
-    calculator myCalc = new calculator();
-    int result = myCalc.add(10, 50);
+
+    @Test
+    public void testAdd() {
+        calculator myCalc = new calculator();
+        int result = myCalc.add(10, 50);
+        assertEquals(60, result, 0);
+    }
 
 }
