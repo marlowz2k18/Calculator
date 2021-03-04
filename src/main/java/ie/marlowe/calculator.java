9f7de1b212cd6fdafb8418e224a570c652d3f398
Marlowe <g00325096@gmit.ie>
2021-03-04 09:57:40 +0000

    Code change

--- src/main/java/ie/marlowe/calculator.java
+++ src/main/java/ie/marlowe/calculator.java
@@ -3,7 +3,15 @@ package ie.marlowe;
 
 public class calculator {
     // This method will add two numbers.
-    public int add(int num1, int num2){
+    public int add(int num1, int num2) {
         return num1 + num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    
+    public  int divide(int num1, int num2){
+      return num1/num2;
     }
 }
--- src/test/java/ie/marlowe/CalculatorTest.java
+++ src/test/java/ie/marlowe/CalculatorTest.java
@@ -1,16 +1,38 @@
 package ie.marlowe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private calculator c;
+
+    @BeforeEach
+    void init(){
+        c = new calculator();
+    }
+
     @Test
     public void testAdd() {
-        calculator myCalc = new calculator();
-        int result = myCalc.add(10, 50);
+        c = new calculator();
+        int result = c.add(10, 50);
         assertEquals(60, result, 0);
+    }
+
+    @Test
+    public void testMult(){
+        c = new calculator();
+        int result1 = c.multiply(5, 4);
+        assertEquals(20, result1, 0);
+    }
+
+    @Test
+    public void testDivide(){
+        c = new calculator();
+        int result2 = c.divide(20, 10);
+        assertEquals(2, result2, 0);
     }
 
 }
