c0c751c1e0b0ab86ec034e9b77c77ecb0d87b74e
Marlowe <g00325096@gmit.ie>
2021-03-04 10:49:01 +0000

    Code change

--- src/main/java/ie/marlowe/calculator.java
+++ src/main/java/ie/marlowe/calculator.java
@@ -12,6 +12,12 @@ public class calculator {
     }
     
     public  int divide(int num1, int num2){
-      return num1/num2;
+       if(num1 > 0 && num2 > 0){
+           int total = num1 / num2;
+           return total;
+       }else {
+           throw new IllegalArgumentException("Invalid number");
+       }
+
     }
 }
--- src/test/java/ie/marlowe/CalculatorTest.java
+++ src/test/java/ie/marlowe/CalculatorTest.java
@@ -1,9 +1,11 @@
 package ie.marlowe;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
 
@@ -14,6 +16,7 @@ public class CalculatorTest {
         c = new calculator();
     }
 
+    @DisplayName("Add two number 10 and 50")
     @Test
     public void testAdd() {
         c = new calculator();
@@ -21,6 +24,14 @@ public class CalculatorTest {
         assertEquals(60, result, 0);
     }
 
+
+//    @Test
+//    public void testAddFail(){
+//        c = new calculator();
+//        assertThrows(IllegalArgumentException.class, () -> c.add(5, 4));
+//    }
+
+    @DisplayName("Multiply by 5 and 4")
     @Test
     public void testMult(){
         c = new calculator();
@@ -28,11 +39,23 @@ public class CalculatorTest {
         assertEquals(20, result1, 0);
     }
 
+//    @Test
+//    public void testMultFail(){
+//        c = new calculator();
+//        assertThrows(IllegalArgumentException.class, () -> c.multiply(10, 6));
+//    }
+
     @Test
     public void testDivide(){
         c = new calculator();
         int result2 = c.divide(20, 10);
         assertEquals(2, result2, 0);
+    }
+
+    @Test
+    public void testDivideFail(){
+        c = new calculator();
+        assertThrows(IllegalArgumentException.class, () -> c.divide(0, 4));
     }
 
 }
